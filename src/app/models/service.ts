3a4f653ef2253a367285b482a9f8e25767ddf6f3
Fabrice BENIMANA <fabrice.benimana@aa.com>
2018-05-06 10:23:33 -0500

    adding the ServiceDetailComponent and refactoring the sercice List component

---
+++ src/app/models/service.ts
@@ -0,0 +1,11 @@
+
+export class Service {
+  public title: string
+  public description: string
+  public imagerUrl: string
+  constructor( title: string,  description: string,  imagerUrl: string) {
+    this.title = title;
+    this.description = description;
+    this.imagerUrl = imagerUrl
+  }
+}
